3dd9e956f1d9ec97283ddccd35d7b25250618012
飞翔的荷兰人 <2818714086@qq.com>
2025-08-04 16:12:49 +0800

    新增表user_prompt

--- models/__init__.py
+++ models/__init__.py
@@ -1,12 +1,14 @@
 
 from .qw import QwAccessToken
 from .ai import Client, Session, SessionRole
-from .user import User
+from .user import User,UserPrompt
+
 
 __all__ = [
     "QwAccessToken",
     "Client",
     "Session",
     "SessionRole",
-    "User"
+    "User",
+    "UserPrompt"
 ]
--- models/user.py
+++ models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, Text
 
 from models.base import Base, TimestampMixin
 
@@ -11,3 +11,13 @@ class User(Base, TimestampMixin):
     nickname = Column(String, comment="昵称")
     birth_year = Column(Integer, comment="出生年")
     gender = Column(Integer, comment="0男/1女")
+
+class UserPrompt(Base,TimestampMixin):
+    __tablename__ = "user_prompt"
+
+
+    id = Column(String, primary_key=True, default=TimestampMixin.generate_id())
+    user_id = Column(String,comment="用户id")
+    prompt = Column(Text,comment="人设")
+    title = Column(String,comment="标题")
+
