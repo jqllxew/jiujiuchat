0c7f4cb458037bf3a6a906f29ef6d699ef0ebad6
jqllxew <a787841563@gmail.com>
2025-08-08 11:33:35 +0800

    chore: 分页

--- services/base.py
+++ services/base.py
@@ -1,5 +1,7 @@
+from typing import Any, Sequence
+
 from redis.asyncio import Redis
-from sqlalchemy import Select
+from sqlalchemy import Select, select, func
 from sqlalchemy.ext.asyncio import AsyncSession
 
 
@@ -15,3 +17,12 @@ class BaseService:
     async def select_list(self, stmt: Select):
         result = await self.db.execute(stmt)
         return result.scalars().all()
+
+    async def select_page(self, stmt: Select, page: int = 1, limit: int = 10) -> tuple[Sequence[Any], int]:
+        total = await self.select_first(
+            select(func.count()).select_from(stmt.subquery())
+        )
+        if total and isinstance(total, int):
+            result_list = await self.select_list(stmt.limit(limit).offset((page-1) * limit))
+            return result_list, total
+        return [], 0
